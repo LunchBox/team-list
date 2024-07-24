bed198f12f06f28f09faa5a11f859e12b0063210
daniel cheang <daniel@cpttm.org.mo>
2024-07-24 19:55:02 +0800

    tiny fix

--- src/views/gantt/GanttView.vue
+++ src/views/gantt/GanttView.vue
@@ -19,6 +19,25 @@ const props = defineProps(["list"]);
 const editMode = ref(false);
 const cellWidth = ref(32);
 
+const layout = ref("day");
+
+const layoutInitializer = {
+  day: () => {
+    cellWidth.value = 32;
+  },
+  week: () => {
+    cellWidth.value = 24;
+  },
+  month: () => {
+    cellWidth.value = 16;
+  },
+};
+
+const switchLayout = (layoutName) => {
+  layout.value = layoutName;
+  layoutInitializer[layoutName]();
+};
+
 // only show items that have start and end date
 const scheduledList = computed(() => {
   return props.list.filter((t) => t.start_date && t.end_date);
@@ -59,17 +78,28 @@ const selectedDate = ref(null);
 </script>
 <template>
   <div class="gantt-view" :style="generalStyle">
+    <div class="before-aside">
+      <slot name="before-aside"></slot>
+    </div>
+
     <div class="aside">
       <div class="row">
         mode: <strong>{{ editMode ? "Edit" : "View" }}</strong> &middot;
-        <a href="#" @click.prevent="editMode = !editMode"> toggle </a>
+        <button @click.prevent="editMode = !editMode">toggle</button>
       </div>
       <div class="row">&nbsp;</div>
       <slot name="aside"></slot>
     </div>
 
     <div class="before-container">
-      <slot name="before-container"></slot>
+      <div class="flex">
+        <slot name="before-container"></slot>
+        <div style="margin-left: auto">
+          <button @click.prevent="switchLayout('day')">day</button>
+          <button @click.prevent="switchLayout('week')">week</button>
+          <button @click.prevent="switchLayout('month')">month</button>
+        </div>
+      </div>
     </div>
 
     <div
@@ -89,13 +119,35 @@ const selectedDate = ref(null);
       </template>
 
       <!-- days in header -->
-      <div
-        class="cell day"
-        :class="{ weekend: isWeekend(d) }"
-        v-for="d in dates"
-      >
-        {{ d.getDate() }}
-      </div>
+      <template v-if="layout === 'day'">
+        <div
+          v-for="d in dates"
+          class="cell day"
+          :class="{ weekend: isWeekend(d) }"
+        >
+          <span>{{ d.getDate() }}</span>
+        </div>
+      </template>
+
+      <template v-if="layout === 'week--'">
+        <div
+          v-for="d in dates"
+          class="cell day"
+          :class="{ weekend: isWeekend(d) }"
+        >
+          <span>{{ d.getDate() }}</span>
+        </div>
+      </template>
+
+      <template v-if="layout === 'month'">
+        <div
+          v-for="d in dates"
+          class="cell day"
+          :class="{ weekend: isWeekend(d) }"
+        >
+          <span>{{ d.getDate() }}</span>
+        </div>
+      </template>
 
       <!-- today marker -->
       <GridColumn
@@ -119,7 +171,7 @@ const selectedDate = ref(null);
         :startDate="startDate"
         :date="d"
         :title="formatDate(d)"
-        @click="selectedDate = d"
+        @mousedown="selectedDate = d"
         @drop="onDropToDate(d)"
         @dragover.prevent
       ></GridColumn>
@@ -180,6 +232,11 @@ strong {
 
   align-items: stretch;
   line-height: var(--line-height);
+}
+
+.before-aside {
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
 }
 
 .aside {
