3f42bc088c8370db3757e074df87d7084483c90a
daniel cheang <daniel@cpttm.org.mo>
2024-07-26 09:09:08 +0800

    remove base from vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/007_module_c",
+  // base: "/007_module_c",
   plugins: [vue()],
   resolve: {
     alias: {
