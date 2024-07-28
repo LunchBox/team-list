86e9419848e4af0ad3040fc1b8be869dbb3f21b2
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2024-07-28 15:58:12 +0800

    fix days diff func

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -7,16 +7,16 @@ export const offsetDate = (date, offset) => {
 };
 
 export const daysDiff = (d1, d2) => {
-  const t = new Date(d1);
-  const f = new Date(d2);
+  const t = new Date(formatDate(d1));
+  const f = new Date(formatDate(d2));
   return (t - f) / 3600 / 24 / 1000;
 };
 
 export const formatDate = (date) => {
   if (date instanceof Date) {
-    return moment(date).format("YYYY-MM-DD").toString();
+    return date.toISOString().slice(0, 10);
   } else {
-    return date;
+    return new Date(date).toISOString().slice(0, 10);
   }
 };
 
